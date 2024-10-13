0729ad72f2762e6a152aeaa9287bea9cda9f8f4c
SANKET-023 <156884315+SANKET-023@users.noreply.github.com>
2024-10-13 21:19:05 +0530

    Update settings.py

--- gas_service_app/gas_service_app/settings.py
+++ gas_service_app/gas_service_app/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9bw$7w9vw&ch)4+(u+5gdh=@-3b$6_69gc4rr7!nxazz&wy!61'
+SECRET_KEY = 'REDACTED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
